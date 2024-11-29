d7410fc6190fdbd3c04a3fb2ebb3ae8a3c2676f2
Alex <alexwang409@gmail.com>
2024-11-29 13:56:42 -0500

    first version of chatbot

--- main.py
+++ main.py
@@ -1,26 +1,44 @@
+from groq import Groq
 import streamlit as st
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
 
-st.title("Echo Bot")
+st.title("ChatGPT-like clone")
 
-# Initialize chat history
+# load environment variables
+load_dotenv()
+groq_api_key = os.getenv("GROQ_API_KEY")
+
+# initialize the OpenAI client
+client = OpenAI(
+    base_url="https://api.groq.com/openai/v1",
+    api_key=groq_api_key,
+)
+
+if "groq_model" not in st.session_state:
+    st.session_state["openai_model"] = "mixtral-8x7b-32768"
+
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
-# Display chat messages from history on app rerun
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-# React to user input
 if prompt := st.chat_input("What is up?"):
-    # Display user message in chat message container
-    st.chat_message("user").markdown(prompt)
-    # Add user message to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
 
-    response = f"Echo: {prompt}"
-    # Display assistant response in chat message container
     with st.chat_message("assistant"):
-        st.markdown(response)
-    # Add assistant response to chat history
+        stream = client.chat.completions.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        )
+        response = st.write_stream(stream)
     st.session_state.messages.append({"role": "assistant", "content": response})
